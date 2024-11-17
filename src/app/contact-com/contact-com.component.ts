e6ec30d7ca91758efeccee1d8379da88219c583d
Sonal Salgar <salgarsonal@gmail.com>
2024-11-17 16:02:58 +0530

    first commit

---
+++ src/app/contact-com/contact-com.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-com',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-com.component.html',
+  styleUrl: './contact-com.component.css'
+})
+export class ContactComComponent {
+
+}
---
+++ src/app/timetable-com/timetable-com.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-com',
+  standalone: true,
+  imports: [],
+  templateUrl: './timetable-com.component.html',
+  styleUrl: './timetable-com.component.css'
+})
+export class TimetableComComponent {
+
+}
